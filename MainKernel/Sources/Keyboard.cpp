8df496e9bb420c28adbc3b866ba8f0809ead8b74
Juha3141 <ianisnumber2027@gmail.com>
2022-10-27 23:49:21 +0900

    Remade interrupt handler to fix the bug

--- MainKernel/Sources/Keyboard.cpp
+++ MainKernel/Sources/Keyboard.cpp
@@ -1,17 +1,8 @@
 #include <Keyboard.hpp>
 
-__attribute__ ((naked)) void Kernel::Keyboard::InterruptHandler(void) {
-    __asm__ ("push rbp");
-    __asm__ ("mov rbp , rsp");
-
-    SAVE_REGISTERS_TO_STACK();
-
-
+void Kernel::Keyboard::MainInterruptHandler(void) {
+    Kernel::printf("A");
     IO::Read(0x60);
-    IO::Write(0x20 , 0x20);
-    
-    LOAD_REGISTERS_FROM_STACK();
 
-    __asm__ ("leave");
-    __asm__ ("iretq");
+    IO::Write(0x20 , 0x20);
 }
