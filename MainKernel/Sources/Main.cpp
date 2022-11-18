88caf3ce5e0d43b88153ad1a8f83d4c9a24ff276
Juha3141 <ianisnumber2027@gmail.com>
2022-11-19 02:07:50 +0900

    Changed the graphic demo

--- MainKernel/Sources/Main.cpp
+++ MainKernel/Sources/Main.cpp
@@ -4,6 +4,8 @@
 
 #include <Graphics/Graphic.hpp>
 
+void GraphicModeDemo(void);
+
 extern "C" void Main(void) {
     /*
     int i;
@@ -58,47 +60,8 @@ extern "C" void Main(void) {
     */
     Kernel::printf("Enabling Interrupt.\n");
     __asm__ ("sti");
-    int i = 0;
-    int X;
-    int Y;
-    unsigned int Color[3] = {0xFF0000 , 0x00FF00 , 0x0000FF};
-    struct Kernel::Mouse::MouseData MouseData;
-    Graphics::Layer Layer1;
-    Graphics::Layer Layer2;
-    Graphics::Layer Layer3;
-    Layer1.Initialize(0 , 0 , 1024 , 768);
-    Layer2.Initialize(512-36 , 334-36 , 72 , 16);
-    Layer3.Initialize(300 , 300 , 300 , 200);
-    Layer1.DrawRectangle(0 , 0 , 1024/2 , 768/2 , 0xFF0000);
-    Layer1.DrawRectangle(1024/2 , 0 , 1024 , 768/2 , 0x00FF00);
-    Layer1.DrawRectangle(0 , 768/2 , 1024/2 , 768 , 0x0000FF);
-    Layer1.DrawRectangle(1024/2 , 768/2 , 1024 , 768 , 0xFFFFFF);
-    Layer2.DrawRectangle(0 , 0 , 72 , 16 , 0xFFFFFF);
-    
-    for(i = 0; i < 200; i += 200/10) {
-        Layer3.DrawRectangle(0 , i , 300 , i+(200/10) , 0xAAAAAA);
-    }
 
-    Layer1.Register();
-    Layer2.Register();
-    Layer3.Register();
-    
-    X = 512-4;
-    Y = 334-4;
-
-    Layer2.DrawText(0 , 0 , 0x00 , "%d" , i);  // Add redrawing system
-    Graphics::UpdateLayer(&(Layer1));
-    while(1) {
-        if(Kernel::Mouse::GetMouseDataQueue(&(MouseData)) == true) {
-            X += MouseData.RelativeX;
-            Y += MouseData.RelativeY;
-            Layer2.DrawRectangle(0 , 0 , 72 , 16 , 0xFFFFFF);
-            Layer2.DrawText(0 , 0 , 0x00 , "%d" , i);  // Add redrawing system
-            
-            Layer2.Move(X , Y);
-            i++;
-        }
-    }
+    GraphicModeDemo();
     while(1) {
         ;
     }
@@ -116,5 +79,72 @@ extern "C" void APStartup(void) {
     
     while(1) {
         
+    }
+}
+
+void GraphicModeDemo(void) {
+    int i = 0;
+    int L2X;
+    int L2Y;
+    int L3X;
+    int L3Y;
+    int L4X;
+    int L4Y;
+    unsigned int Color[3] = {0xFF0000 , 0x00FF00 , 0x0000FF};
+    struct Kernel::Mouse::MouseData MouseData;
+    Graphics::Layer Layer1;
+    Graphics::Layer Layer2;
+    Graphics::Layer Layer3;
+    Graphics::Layer Layer4;
+    Layer1.Initialize(0 , 0 , 1024 , 768);
+    Layer2.Initialize(512-36 , 334-36 , 72 , 16);
+    Layer3.Initialize(300 , 300 , 300 , 200);
+    Layer4.Initialize(400 , 400 , 350 , 350);
+    Layer1.DrawRectangle(0 , 0 , 1024/2 , 768/2 , 0xFF0000);
+    Layer1.DrawRectangle(1024/2 , 0 , 1024 , 768/2 , 0x00FF00);
+    Layer1.DrawRectangle(0 , 768/2 , 1024/2 , 768 , 0x0000FF);
+    Layer1.DrawRectangle(1024/2 , 768/2 , 1024 , 768 , 0xFFFFFF);
+
+    Layer2.DrawRectangle(0 , 0 , 36 , 16 , 0xB2CCFF);
+    Layer2.DrawRectangle(36 , 0 , 72 , 16 , 0xB2EBF4);
+
+    Layer3.DrawRectangle(0 , 0 , 300 , 200 , 0x000000);
+    Layer3.DrawRectangle(5 , 5 , 300-5 , 200-5 , 0x222222);
+    
+    Layer4.DrawRectangle(0 , 0 , 350 , 350 , 0x000000);
+    Layer4.DrawRectangle(5 , 5 , 350-5 , 350-5 , 0xFFFFFF);
+
+
+    Layer1.Register();
+    Layer2.Register();
+    Layer3.Register();
+    Layer4.Register();
+    
+    L2X = 512-4;
+    L2Y = 334-4;
+    L3X = 300;
+    L3Y = 300;
+    L4X = 400;
+    L4Y = 400;
+    Graphics::UpdateLayer(Layer1);
+    while(1) {
+        if(Kernel::Mouse::GetMouseDataQueue(&(MouseData)) == true) {
+            if((MouseData.ButtonData & MOUSE_BUTTONLEFT) == MOUSE_BUTTONLEFT) {
+                L2X += MouseData.RelativeX;
+                L2Y += MouseData.RelativeY;
+                Layer2.Move(L2X , L2Y);
+            }
+            if((MouseData.ButtonData & MOUSE_BUTTONRIGHT) == MOUSE_BUTTONRIGHT) {
+                L3X += MouseData.RelativeX;
+                L3Y += MouseData.RelativeY;
+                Layer3.Move(L3X , L3Y);
+            }
+            if((MouseData.ButtonData & MOUSE_BUTTONMIDDLE) == MOUSE_BUTTONMIDDLE) {
+                L4X += MouseData.RelativeX;
+                L4Y += MouseData.RelativeY;
+                Layer4.Move(L4X , L4Y);
+            }
+            i++;
+        }
     }
 }
