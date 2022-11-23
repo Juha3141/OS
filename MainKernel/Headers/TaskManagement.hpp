5091a70ac50097a187d3f9af1a756ec8dd41a38c
Juha3141 <ianisnumber2027@gmail.com>
2022-11-24 00:30:35 +0900

    Added basic task management system based on MLQ Algorithm

---
+++ MainKernel/Headers/TaskManagement.hpp
@@ -0,0 +1,131 @@
+#ifndef _SCHEDULER_HPP_
+#define _SCHEDULER_HPP_
+
+#include <Kernel.hpp>
+
+#define TASK_COUNT_PER_QUEUE          256
+
+#define TASK_FLAGS_PRIVILAGE_KERNEL         0x00
+#define TASK_FLAGS_PRIVILAGE_DEVICE_DRIVER1 0x01
+#define TASK_FLAGS_PRIVILAGE_DEVICE_DRIVER2 0x02
+#define TASK_FLAGS_PRIVILAGE_USER_LEVEL     0x03
+#define TASK_FLAGS_WORKING                  0x04
+#define TASK_FLAGS_PAUSED                   0x08
+#define TASK_FLAGS_SEPERATED_MEMORY_AREA    0x10
+
+#define TASK_STATUS_WORKING           0x01
+#define TASK_STATUS_PAUSED            0x02
+
+#define TASK_STACK_SIZE               8*1024*1024
+
+#define TASK_MAX_DEMANDED_TIME        50
+#define TASK_PRIORITY_COUNT           10
+
+namespace Kernel {
+    struct TaskRegisters {
+        unsigned long RAX;
+        unsigned long RBX;
+        unsigned long RCX;
+        unsigned long RDX;
+        
+        unsigned long RSI;
+        unsigned long RDI;
+
+        unsigned long R8;
+        unsigned long R9;
+        unsigned long R10;
+        unsigned long R11;
+        unsigned long R12;
+        unsigned long R13;
+        unsigned long R14;
+        unsigned long R15;
+
+        unsigned long RSP;
+        unsigned long RBP;
+
+        unsigned long RIP;
+
+        unsigned long CS;
+        unsigned long DS;
+        unsigned long ES;
+        unsigned long FS;
+        unsigned long GS;
+        unsigned long SS;
+
+        unsigned long RFlags;
+        unsigned long CR3;
+    };
+    namespace TaskManagement {
+        struct Task {
+            unsigned long ID;
+            unsigned long Flags;
+            unsigned long Priority;
+            
+            struct TaskRegisters Registers;
+            unsigned long CR3;
+            
+            char Name[32];
+
+            struct Task *NextTask;
+            struct Task *PreviousTask;
+        };
+
+        class PriorityQueue {
+            friend class SchedulingManager;
+            friend class PriorityQueue;
+            public:
+                void Initialize(int Time);
+                void AddTask(struct Task *Task);
+                void RemoveTask(unsigned long TaskID);
+
+                struct Task *GetCurrentTask(void);
+                void SwitchToNextTask(void);
+                
+                int RunningTime;
+            private:
+                struct Task *StartTask;
+                struct Task *CurrentTask;
+                int TaskCount = 0;
+        };
+        class SchedulingManager {
+            friend class PriorityQueue;
+            public:
+                void Initialize(void);
+                void AddTaskToPriorityQueue(struct Task *Task);
+                struct Task *SwitchTask(void);
+
+                bool IsTaskDone(void) {
+                    if(ExpirationDate <= 0) {
+                        return true;
+                    }
+                    return false;
+                }
+                void SlowlyExpirate(void) {
+                    ExpirationDate -= 1;
+                }
+                void SetExpirationDate(int Time) {
+                    ExpirationDate = Time;
+                }
+
+                struct Task *CurrentlyRunningTask;
+                int CurrentMaxAllocatedID = 0x00;
+            private:
+                PriorityQueue *PriorityQueues;
+                int TotalTaskCount;
+                int CurrentPriority = 0;
+
+                int ExpirationDate;
+        };
+
+        void Initialize(void);
+        unsigned long CreateTask(unsigned long StartAddress , unsigned long Flags , unsigned long Priority , const char *TaskName);
+        void SwitchTask(void);
+        void SwitchTaskInTimerInterrupt(void);
+        struct Task *GetCurrentlyRunningTask(void);
+        unsigned long GetCurrentlyRunningTaskID(void);
+        
+        extern "C" void SwitchContext(struct Kernel::TaskRegisters *LastContext , struct Kernel::TaskRegisters *ContextToChange);
+    }
+}
+
+#endif
---
+++ MainKernel/Sources/TaskManagement.cpp
@@ -0,0 +1,268 @@
+#include <TaskManagement.hpp>
+
+static Kernel::TaskManagement::SchedulingManager *TaskSchedulingManager;
+
+static void SetTaskRegisters(struct Kernel::TaskManagement::Task *Task , unsigned long StartAddress);
+
+void Kernel::TaskManagement::Initialize(void) {
+    TaskSchedulingManager = (SchedulingManager *)Kernel::SystemStructure::Allocate(sizeof(SchedulingManager));
+    Kernel::printf("Address : 0x%X\n" , TaskSchedulingManager);
+    TaskSchedulingManager->Initialize();
+} 
+
+static void SetTaskRegisters(struct Kernel::TaskManagement::Task *Task , unsigned long StartAddress) {
+    memset(&(Task->Registers) , 0 , sizeof(struct Kernel::TaskRegisters));
+    switch(Task->Flags & 0x02) {
+        case TASK_FLAGS_PRIVILAGE_KERNEL:
+            Task->Registers.CS = KERNEL_CS;
+            Task->Registers.DS = KERNEL_DS;
+            Task->Registers.ES = KERNEL_DS;
+            Task->Registers.FS = KERNEL_DS;
+            Task->Registers.GS = KERNEL_DS;
+            Task->Registers.SS = KERNEL_DS;
+            break;
+        case TASK_FLAGS_PRIVILAGE_DEVICE_DRIVER1:
+            Task->Registers.CS = RING1_CS;
+            Task->Registers.DS = RING1_DS;
+            Task->Registers.ES = RING1_DS;
+            Task->Registers.FS = RING1_DS;
+            Task->Registers.GS = RING1_DS;
+            Task->Registers.SS = RING1_DS;
+            break;
+        case TASK_FLAGS_PRIVILAGE_DEVICE_DRIVER2:
+            Task->Registers.CS = RING2_CS;
+            Task->Registers.DS = RING2_DS;
+            Task->Registers.ES = RING2_DS;
+            Task->Registers.FS = RING2_DS;
+            Task->Registers.GS = RING2_DS;
+            Task->Registers.SS = RING2_DS;
+            break;
+        case TASK_FLAGS_PRIVILAGE_USER_LEVEL:
+            Task->Registers.CS = USER_CS;
+            Task->Registers.DS = USER_DS;
+            Task->Registers.ES = USER_DS;
+            Task->Registers.FS = USER_DS;
+            Task->Registers.GS = USER_DS;
+            Task->Registers.SS = USER_DS;
+            break;
+    }
+    Task->Registers.RSP = (unsigned long)Kernel::MemoryManagement::Allocate(TASK_STACK_SIZE)+TASK_STACK_SIZE-8;
+    Task->Registers.RBP = Task->Registers.RSP;
+    Task->Registers.RFlags = 0x202;
+    Task->Registers.RIP = StartAddress;
+    // Temprory
+    __asm__ ("mov %0 , cr3":"=r"(Task->Registers.CR3));
+}
+
+unsigned long Kernel::TaskManagement::CreateTask(unsigned long StartAddress , unsigned long Flags , unsigned long Priority , const char *TaskName) {
+    struct Task *Task;
+    Task = (struct Task *)Kernel::MemoryManagement::Allocate(sizeof(struct Task));
+    Task->ID = TaskSchedulingManager->CurrentMaxAllocatedID++;
+    Task->Flags = Flags;
+    Task->Priority = Priority;
+
+    SetTaskRegisters(Task , StartAddress);
+    strcpy(Task->Name , TaskName);
+
+    TaskSchedulingManager->AddTaskToPriorityQueue(Task);
+    return Task->ID;
+}
+
+void Kernel::TaskManagement::SchedulingManager::Initialize(void) {
+    int i;
+    int RunningTimePerPriority = TASK_MAX_DEMANDED_TIME;
+    struct Task *MainTask;
+    TotalTaskCount = 0;
+    PriorityQueues = (TaskManagement::PriorityQueue *)Kernel::MemoryManagement::Allocate(TASK_PRIORITY_COUNT*sizeof(PriorityQueue));
+    for(i = 0; i < TASK_PRIORITY_COUNT-1; i++) {
+        PriorityQueues[i].Initialize(TASK_PRIORITY_COUNT-i);
+        Kernel::printf("Time Demanded to Priority %d : %d cycle\n" , i , TASK_PRIORITY_COUNT-i);
+    }
+    MainTask = (struct Task *)Kernel::MemoryManagement::Allocate(sizeof(struct Task));
+    MainTask->ID = this->CurrentMaxAllocatedID++;
+    MainTask->Flags = TASK_FLAGS_PRIVILAGE_KERNEL|TASK_FLAGS_WORKING;
+    MainTask->Priority = 0;
+
+    SetTaskRegisters(MainTask , 0x00);
+    strcpy(MainTask->Name , "MAINTASK");
+
+    PriorityQueues[0].AddTask(MainTask);
+    CurrentlyRunningTask = MainTask;
+    Kernel::printf("Main Task ID : %d\n" , MainTask->ID);
+}
+
+void Kernel::TaskManagement::SchedulingManager::AddTaskToPriorityQueue(struct Task *Task) {
+    PriorityQueues[Task->Priority].AddTask(Task);
+}
+
+struct Kernel::TaskManagement::Task *Kernel::TaskManagement::SchedulingManager::SwitchTask(void) {
+    struct Task *Task;
+    if(CurrentPriority >= TASK_PRIORITY_COUNT) {
+        CurrentPriority = 0;
+    }
+    if(PriorityQueues[CurrentPriority].TaskCount != 0) {
+        PriorityQueues[CurrentPriority].SwitchToNextTask();
+        Task = PriorityQueues[CurrentPriority].GetCurrentTask();
+        this->CurrentlyRunningTask = Task;
+        
+        TaskSchedulingManager->SetExpirationDate(PriorityQueues[CurrentPriority].RunningTime);
+        CurrentPriority++;
+        
+        // To-do : We need to demand time for each priority queue!!
+
+    }
+    else {
+        CurrentPriority++;
+        return this->SwitchTask();
+    }
+    return Task;
+}
+
+void Kernel::TaskManagement::SwitchTask(void) {
+    struct Kernel::TaskManagement::Task *PreviousTask;
+    struct Kernel::TaskManagement::Task *CurrentTask;
+    PreviousTask = TaskSchedulingManager->CurrentlyRunningTask;
+    if(TaskSchedulingManager->IsTaskDone() == false) {
+        TaskSchedulingManager->SlowlyExpirate();
+        return;
+    }
+    CurrentTask = TaskSchedulingManager->SwitchTask();
+    
+    SwitchContext(&(PreviousTask->Registers) , &(CurrentTask->Registers));
+}
+
+void Kernel::TaskManagement::SwitchTaskInTimerInterrupt(void) {
+    struct Kernel::TaskManagement::Task *PreviousTask;
+    struct Kernel::TaskManagement::Task *CurrentTask;
+    struct Kernel::STACK_STRUCTURE *IST = (struct Kernel::STACK_STRUCTURE *)(IST_STARTADDRESS+IST_SIZE-sizeof(struct Kernel::STACK_STRUCTURE));
+    PreviousTask = TaskSchedulingManager->CurrentlyRunningTask;
+    if(TaskSchedulingManager->IsTaskDone() == false) {
+        TaskSchedulingManager->SlowlyExpirate();
+        return;
+    }
+    CurrentTask = TaskSchedulingManager->SwitchTask();
+
+    PreviousTask->Registers.RAX = IST->RAX;
+    PreviousTask->Registers.RBX = IST->RBX;
+    PreviousTask->Registers.RCX = IST->RCX;
+    PreviousTask->Registers.RDX = IST->RDX;
+    
+    PreviousTask->Registers.RDI = IST->RDI;
+    PreviousTask->Registers.RSI = IST->RSI;
+    
+    PreviousTask->Registers.R8 = IST->R8;
+    PreviousTask->Registers.R9 = IST->R9;
+    PreviousTask->Registers.R10 = IST->R10;
+    PreviousTask->Registers.R11 = IST->R11;
+    PreviousTask->Registers.R12 = IST->R12;
+    PreviousTask->Registers.R13 = IST->R13;
+    PreviousTask->Registers.R14 = IST->R14;
+    PreviousTask->Registers.R15 = IST->R15;
+
+    PreviousTask->Registers.RIP = IST->RIP;
+    PreviousTask->Registers.RBP = IST->RBP;
+    PreviousTask->Registers.RSP = IST->RSP;
+    
+    PreviousTask->Registers.CS = IST->CS;
+    PreviousTask->Registers.DS = IST->DS;
+    PreviousTask->Registers.ES = IST->ES;
+    PreviousTask->Registers.FS = IST->FS;
+    PreviousTask->Registers.GS = IST->GS;
+    PreviousTask->Registers.SS = IST->SS;
+
+    PreviousTask->Registers.RFlags = IST->RFlags;
+    
+    IST->RAX = CurrentTask->Registers.RAX;
+    IST->RBX = CurrentTask->Registers.RBX;
+    IST->RCX = CurrentTask->Registers.RCX;
+    IST->RDX = CurrentTask->Registers.RDX;
+    
+    IST->RDI = CurrentTask->Registers.RDI;
+    IST->RSI = CurrentTask->Registers.RSI;
+    
+    IST->R8 = CurrentTask->Registers.R8;
+    IST->R9 = CurrentTask->Registers.R9;
+    IST->R10 = CurrentTask->Registers.R10;
+    IST->R11 = CurrentTask->Registers.R11;
+    IST->R12 = CurrentTask->Registers.R12;
+    IST->R13 = CurrentTask->Registers.R13;
+    IST->R14 = CurrentTask->Registers.R14;
+    IST->R15 = CurrentTask->Registers.R15;
+
+    IST->CS = CurrentTask->Registers.CS;
+    IST->ES = CurrentTask->Registers.ES;
+    IST->FS = CurrentTask->Registers.FS;
+    IST->GS = CurrentTask->Registers.GS;
+    IST->SS = CurrentTask->Registers.SS;
+
+    IST->RFlags = CurrentTask->Registers.RFlags;
+    
+    IST->RIP = CurrentTask->Registers.RIP;
+    IST->RBP = CurrentTask->Registers.RBP;
+    IST->RSP = CurrentTask->Registers.RSP;
+}
+
+struct Kernel::TaskManagement::Task *Kernel::TaskManagement::GetCurrentlyRunningTask(void) {
+    return TaskSchedulingManager->CurrentlyRunningTask;
+}
+
+unsigned long Kernel::TaskManagement::GetCurrentlyRunningTaskID(void) {
+    return TaskSchedulingManager->CurrentlyRunningTask->ID;
+}
+
+////////// PriorityQueue ////////////
+
+void Kernel::TaskManagement::PriorityQueue::Initialize(int Time) {
+    RunningTime = Time;
+}
+
+void Kernel::TaskManagement::PriorityQueue::AddTask(struct Task *Task) {
+    struct Task *TaskPointer;
+    if(TaskCount == 0) {
+        StartTask = Task;
+        StartTask->NextTask = StartTask;
+        StartTask->PreviousTask = 0x00;
+        TaskCount++;
+        CurrentTask = StartTask;
+        return;
+    }
+    TaskPointer = StartTask;
+    while(TaskPointer->NextTask != StartTask) {
+        TaskPointer = TaskPointer->NextTask;
+    }
+    TaskPointer->NextTask = Task;
+    TaskPointer->NextTask->NextTask = StartTask;
+    TaskPointer->NextTask->PreviousTask = TaskPointer;
+    TaskCount++;
+    return;
+}
+
+void Kernel::TaskManagement::PriorityQueue::RemoveTask(unsigned long TaskID) {
+    struct Task *TaskPointer = StartTask;
+    if(TaskCount == 0) {
+        return;
+    }
+    while(1) {
+        if(TaskPointer->ID == TaskID) {
+            break;
+        }
+        TaskPointer = TaskPointer->NextTask;
+    }
+    if(TaskPointer->PreviousTask == 0) {
+        TaskPointer->NextTask->PreviousTask = 0x00;
+        StartTask = TaskPointer->NextTask;
+    }
+    else {
+        TaskPointer->NextTask->PreviousTask = TaskPointer->PreviousTask;
+        TaskPointer->PreviousTask->NextTask = TaskPointer->NextTask;
+    }
+    TaskCount -= 1;
+}
+
+struct Kernel::TaskManagement::Task *Kernel::TaskManagement::PriorityQueue::GetCurrentTask(void) {
+    return CurrentTask;
+}
+
+void Kernel::TaskManagement::PriorityQueue::SwitchToNextTask(void) {
+    CurrentTask = CurrentTask->NextTask;
+}
