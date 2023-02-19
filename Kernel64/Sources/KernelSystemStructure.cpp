7c6f7e80191a39ba1371df0508a3d44885994157
Juha3141 <ianisnumber2027@gmail.com>
2023-02-20 02:46:11 +0900

    Now system structure area is initialized to reduce potential errors.

--- Kernel64/Sources/KernelSystemStructure.cpp
+++ Kernel64/Sources/KernelSystemStructure.cpp
@@ -13,9 +13,22 @@
 static Kernel::SystemStructure::Manager *StructureManager = (Kernel::SystemStructure::Manager*)KERNELSYSTEMSTRUCTURE_LOCATION;
 
 void Kernel::SystemStructure::Initialize(void) {
+    unsigned long Address;
+    unsigned char *Error = (unsigned char *)0xB8000;
     // Location of system structure manager       : 0x500000
     // Size of system structure manager structure : 8 bytes
     // Start of the memory pool                   : 0x500008 
+    for(Address = KERNELSYSTEMSTRUCTURE_LOCATION; Address < KERNELSYSTEMSTRUCTURE_LOCATION+KERNELSYSTEMSTRUCTURE_SIZE; Address += 8) {
+        *((unsigned long *)Address) = 0x00;
+        if(*((unsigned long *)Address) != 0x00) {
+            Error[0] = 'M';
+            Error[2] = 'E';
+            Error[4] = 'R';
+            while(1) {
+                ;
+            }
+        }
+    }
     StructureManager->CurrentAddress = (KERNELSYSTEMSTRUCTURE_LOCATION+sizeof(Kernel::SystemStructure::Manager));
 }
 
