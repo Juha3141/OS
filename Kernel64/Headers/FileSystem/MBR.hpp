e3334c8c6f99fb57163ab7f2ae513377a69c753d
Juha3141 <ianisnumber2027@gmail.com>
2023-02-24 00:09:30 +0900

    Implemented functions that are able to detect MBR Partitions

--- Kernel64/Headers/FileSystem/MBR.hpp
+++ Kernel64/Headers/FileSystem/MBR.hpp
@@ -8,25 +8,37 @@
 
 namespace Kernel {
 	namespace MBR {
-		struct PartitionTable {
+		struct PartitionTableEntry {
 			unsigned char BootableFlag;
-			unsigned char CHS[3];
+			unsigned char StartingCHS[3];
 			unsigned char PartitionType;
+			unsigned char EndingCHS[3];
+			unsigned int StartingLBA;
+			unsigned int SizeInSector;
+		};
+		struct PartitionTable {
+			unsigned char BootCode[446];
+			struct PartitionTableEntry Entries[4];
+			unsigned short Signature; // Always 0xAA55
 		};
 		class Identifier {
 			public:
-				Identifier(Drivers::StorageSystem::Driver *StorageDriver_ , Drivers::StorageSystem::Storage *Storage_)
-																			 : StorageDriver(StorageDriver_)
-																			 , Storage(Storage_)
-																			 , PartitionCount(0) {}
-				bool DetectMBR(void);
+				Identifier(Drivers::StorageSystem::Driver *StorageDriver_
+				         , Drivers::StorageSystem::Storage *Storage_)
+						 : StorageDriver(StorageDriver_)
+						 , Storage(Storage_)
+						 , PartitionCount(0) {
+					PartitionTable = (struct PartitionTable *)Kernel::MemoryManagement::Allocate(512);
+				}
+				bool Detect(void);
 				Drivers::StorageSystem::Partition *GetPartition(void);
 
 				int PartitionCount;
 			private:
 				Drivers::StorageSystem::Driver *StorageDriver = 0; 
-				Drivers::StorageSystem::Storage *Storage = 0; 
+				Drivers::StorageSystem::Storage *Storage = 0;
 				Drivers::StorageSystem::Partition *Partitions;
+			    struct PartitionTable *PartitionTable;
 
 				unsigned int CurrentOffset = 0;
 		};
--- Kernel64/Sources/FileSystem/MBR.cpp
+++ Kernel64/Sources/FileSystem/MBR.cpp
@@ -3,14 +3,59 @@
 using namespace Kernel;
 using namespace Kernel::Drivers;
 
-bool MBR::Identifier::DetectMBR(void) {
+bool MBR::Identifier::Detect(void) {
+    int i;
+    // Problem : PartitionTable demands more memory than 512B, but I allocated only 512Bytes, so PartitionTable exceeded its demand, 
+    // And violated memory policy. I think that the problem could be ReadSector, because it does not read only 512 bytes requested, it reads more.
+    // So to solve this problem, we have to rewrite ReadSector code to make "only" read requested areas.
     if(StorageDriver == 0x00) {
+        //Kernel::MemoryManagement::Free(PartitionTable);
         return false;
     }
-    return true;
+    if(Storage->Geometry.BytesPerSector != 512) {
+        //Kernel::MemoryManagement::Free(PartitionTable);
+        return false;
+    }
+    if(StorageDriver->ReadSectorFunction(Storage , 0 , 1 , PartitionTable) != 512) {
+        //Kernel::MemoryManagement::Free(PartitionTable);
+        return false;
+    }
+    if(PartitionTable->Signature != 0xAA55) { // To-do : Improve MBR detection system
+        //Kernel::MemoryManagement::Free(PartitionTable);
+        return false;
+    }
+    for(i = 0; i < 4; i++) {
+        if(PartitionTable->Entries[i].PartitionType == 0xEE) { // GPT, not MBR
+            //Kernel::MemoryManagement::Free(PartitionTable);
+            return false;   
+        }
+        if(PartitionTable->Entries[i].PartitionType != 0x00) {
+            Kernel::printf("MBR Detected\n");
+            //Kernel::MemoryManagement::Free(PartitionTable);
+            return true;
+        }
+    }
+    Kernel::MemoryManagement::Free(PartitionTable);
+    return false;
 }
 
 StorageSystem::Partition *MBR::Identifier::GetPartition(void) {
-    // later
-    return 0x00;
+    int i;
+    struct PartitionTable *PartitionTable = (struct PartitionTable *)Kernel::MemoryManagement::Allocate(512);
+    if(StorageDriver->ReadSectorFunction(Storage , 0 , 1 , PartitionTable) != 512) {
+        return 0x00;
+    }
+    Partitions = (StorageSystem::Partition *)Kernel::MemoryManagement::Allocate(4*sizeof(struct StorageSystem::Partition));
+    PartitionCount = 0;
+    for(i = 0; i < 4; i++) {
+        if(PartitionTable->Entries[i].PartitionType != 0) {
+            Partitions[PartitionCount].StartAddressLBA = PartitionTable->Entries[i].StartingLBA;
+            Partitions[PartitionCount].EndAddressLBA = PartitionTable->Entries[i].StartingLBA+PartitionTable->Entries[i].SizeInSector;
+            Partitions[PartitionCount].PartitionType = PartitionTable->Entries[i].PartitionType;
+            Partitions[PartitionCount].IsBootable = ((PartitionTable->Entries[i].BootableFlag == 0x00) ? false : true);
+            PartitionCount++;
+        }
+    }
+    Kernel::printf("(MBR) PartitionCount : %d (Driver %s , Storage %d)\n" , PartitionCount , StorageDriver->DriverName , Storage->ID);
+    return Partitions;
 }
