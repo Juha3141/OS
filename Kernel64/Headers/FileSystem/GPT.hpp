143fced0050cac50975f56ed6118979e4d8b8936
Juha3141 <ianisnumber2027@gmail.com>
2023-02-24 00:10:09 +0900

    Implemented functions that are able to detect GPT Partitions

--- Kernel64/Headers/FileSystem/GPT.hpp
+++ Kernel64/Headers/FileSystem/GPT.hpp
@@ -3,33 +3,67 @@
 
 #include <Kernel.hpp>
 #include <Drivers/StorageDriver.hpp>
+#include <FileSystem/MBR.hpp>
 
-namespace GPT {
-	struct GPTHeader {
-		unsigned char Signature[8];
-		unsigned int Revision;
-		unsigned int HeaderSize;
-		unsigned int CRC32Value;
-		unsigned int Reserved1;
-		unsigned long GPTHeaderLBA;
-		unsigned long BackupGPTHeaderLBA;
-		unsigned long AvailablePartitionLBA;
-		unsigned long EndingAvailPartitionLBA;
-		unsigned char DiskGUID[16];
-		unsigned long PartitionTableEntryLBA;
-		unsigned int NumberOfPartitionEntries;
-		unsigned int PartitionTableEntrySize;
-		unsigned PartitionTableCRC32Value;
-		unsigned char Reserved2[420];
-	};
-	struct PartitionTableEntry {
-		unsigned char PartitionTypeGUID[16];
-		unsigned char UniquePartitionGUID[16];
-		unsigned long StartAddressLBA;
-		unsigned long EndAddressLBA;
-		unsigned long AttributeFlag;
-		// char PartitionName[?]
-	};
+#define GPT_PARTITION_SYSTEM 	       0x00
+#define GPT_PARTITION_EFI_FIRMWARE     0x01
+#define GPT_PARTITION_LEGACY_BOOTABLE  0x02
+#define GPT_PARTITION_READ_ONLY 	   0x60
+#define GPT_PARTITION_SHADOW_COPY      0x61
+#define GPT_PARTITION_HIDDEN	 	   0x62
+#define GPT_PARTITION_NO_DRIVE_LETTEER 0x63
+
+namespace Kernel {
+	namespace GPT {
+		struct GPTHeader {
+			unsigned char Signature[8];
+			unsigned int Revision;
+			unsigned int HeaderSize;
+			unsigned int CRC32Value;
+			unsigned int Reserved1;
+			unsigned long GPTHeaderLBA;
+			unsigned long BackupGPTHeaderLBA;
+			unsigned long AvailablePartitionLBA;
+			unsigned long EndingAvailPartitionLBA;
+			unsigned char DiskGUID[16];
+			unsigned long PartitionTableEntryLBA;
+			unsigned int NumberOfPartitionEntries;
+			unsigned int PartitionTableEntrySize;
+			unsigned PartitionTableCRC32Value;
+			unsigned char Reserved2[420];
+		};
+		struct PartitionTableEntry {
+			unsigned char PartitionTypeGUID[16];
+			unsigned char UniquePartitionGUID[16];
+			unsigned long StartAddressLBA;
+			unsigned long EndAddressLBA;
+			unsigned long AttributeFlag;
+			char PartitionName[72];
+		};
+		class Identifier {
+			public:
+				Identifier(Drivers::StorageSystem::Driver *StorageDriver_
+				         , Drivers::StorageSystem::Storage *Storage_)
+						 : StorageDriver(StorageDriver_)
+						 , Storage(Storage_)
+						 , PartitionCount(0) {
+					Header = (struct GPTHeader *)Kernel::MemoryManagement::Allocate(512);
+				}
+				bool Detect(void);
+				Drivers::StorageSystem::Partition *GetPartition(void);
+
+				int PartitionCount;
+				struct PartitionTableEntry *PartitionTableEntry;
+				struct GPTHeader *Header;
+			private:
+				Drivers::StorageSystem::Driver *StorageDriver = 0; 
+				Drivers::StorageSystem::Storage *Storage = 0;
+				Drivers::StorageSystem::Partition *Partitions;
+
+				unsigned int CurrentOffset = 0;
+
+		};
+	}
 }
 
 #endif
---
+++ Kernel64/Sources/FileSystem/GPT.cpp
@@ -0,0 +1,62 @@
+#include <FileSystem/GPT.hpp>
+
+using namespace Kernel;
+using namespace Kernel::Drivers;
+
+bool GPT::Identifier::Detect(void) {
+    int i;
+    if(StorageDriver == 0x00) {
+        //Kernel::MemoryManagement::Free(Header);
+        return false;
+    }
+    if(Storage->Geometry.BytesPerSector != 512) {
+        //Kernel::MemoryManagement::Free(Header);
+        return false;
+    }
+    if(StorageDriver->ReadSectorFunction(Storage , 1 , 1 , Header) != 512) {
+        //Kernel::MemoryManagement::Free(Header);
+        return false;
+    }
+    if(memcmp(Header->Signature , "EFI PART" , 8) != 0) {
+        //Kernel::MemoryManagement::Free(Header);
+        return false;
+    }
+    Kernel::printf("GPT Detected\n");
+    return true;
+}
+
+StorageSystem::Partition *GPT::Identifier::GetPartition(void) {
+    int i = 0;
+    int j;
+    unsigned int AllocateSize = Header->NumberOfPartitionEntries*sizeof(struct PartitionTableEntry);
+    struct StorageSystem::Partition *Pointer;
+    if((AllocateSize%512) != 0x00) {
+        AllocateSize = (((unsigned int)(AllocateSize/512))+1)*512;
+    }
+    Partitions = (StorageSystem::Partition *)Kernel::MemoryManagement::Allocate(128*sizeof(struct StorageSystem::Partition));
+    PartitionTableEntry = (struct PartitionTableEntry *)Kernel::MemoryManagement::Allocate(AllocateSize);
+    StorageDriver->ReadSectorFunction(Storage , Header->PartitionTableEntryLBA ,  AllocateSize/512 , PartitionTableEntry);
+    if((PartitionTableEntry[0].PartitionTypeGUID[0] == 0) && (PartitionTableEntry[0].PartitionTypeGUID[1] == 0)
+    && (PartitionTableEntry[0].PartitionTypeGUID[2] == 0) && (PartitionTableEntry[0].PartitionTypeGUID[3] == 0)) {
+        i = 1;
+    }
+    PartitionCount = 0;
+    for(j = 0; i < Header->NumberOfPartitionEntries; i++) {
+        if((PartitionTableEntry[i].PartitionTypeGUID[0] == 0) && (PartitionTableEntry[i].PartitionTypeGUID[1] == 0)
+        && (PartitionTableEntry[i].PartitionTypeGUID[2] == 0) && (PartitionTableEntry[i].PartitionTypeGUID[3] == 0)) {
+            break;
+        }
+        Partitions[PartitionCount].StartAddressLBA = PartitionTableEntry[i].StartAddressLBA;
+        Partitions[PartitionCount].EndAddressLBA = PartitionTableEntry[i].EndAddressLBA;
+        Partitions[PartitionCount].PartitionType = PartitionTableEntry[i].AttributeFlag;
+        if(Partitions[PartitionCount].PartitionType == GPT_PARTITION_LEGACY_BOOTABLE) {
+            Partitions[PartitionCount].IsBootable = true;
+        }
+        else {
+            Partitions[PartitionCount].IsBootable = false;
+        }
+        PartitionCount += 1;
+    }
+    Kernel::printf("(GPT) PartitionCount : %d (Driver %s , Storage %d)\n" , PartitionCount , StorageDriver->DriverName , Storage->ID);
+    return Partitions;
+}
