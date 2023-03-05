f0041bf124749f9d8a61d599c138e78b2e0f1467
Juha3141 <ianisnumber2027@gmail.com>
2023-03-05 23:48:40 +0900

    Migrated codes to RAMDisk.hpp

--- Kernel64/Headers/Drivers/BootRAMDisk.hpp
+++ Kernel64/Headers/Drivers/BootRAMDisk.hpp
@@ -2,29 +2,9 @@
 #define _BOOTRAMDISK_HPP_
 
 #define BOOTRAMDISK_ADDRESS          0x720000
-#define BOOTRAMDISK_ENDADDRESS       0x1720000
+#define BOOTRAMDISK_ENDADDRESS       0x2000000
 #define BOOTRAMDISK_BYTES_PER_SECTOR 2048
 
 #define BOOTRAMDISK_SIGNATURE        0xC001DABFACE101FF
-
-#ifndef KERNEL32
-
-#include <Kernel.hpp>
-#include <Drivers/DeviceDriver.hpp>
-#include <Drivers/StorageDriver.hpp>
-
-namespace Kernel {
-    namespace Drivers {
-        namespace BootRAMDisk {
-            void Register(void);
-            bool PreInitialization(StorageSystem::Driver *Driver);
-            bool GetGeometry(StorageSystem::Storage *Storage , StorageSystem::StorageGeometry *Geometry);
-            unsigned long ReadSector(StorageSystem::Storage *Storage , unsigned long SectorAddress , unsigned long Count , void *Buffer);
-            unsigned long WriteSector(StorageSystem::Storage *Storage , unsigned long SectorAddress , unsigned long Count , void *Buffer);
-        }
-    }
-}
-
-#endif
 
 #endif
