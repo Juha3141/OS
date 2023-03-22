0f4627279a61c46e2f9ac507adc2d45ba3f72d7b
Juha3141 <ianisnumber2027@gmail.com>
2023-03-22 15:42:51 +0900

    Core stack seperated, and added some demo codes

--- Kernel64/Sources/Main.cpp
+++ Kernel64/Sources/Main.cpp
@@ -4,6 +4,7 @@
 #include <TaskManagement.hpp>
 #include <ResourceAccessManagement.hpp>
 #include <Paging.hpp>
+#include <MutualExclusion.hpp>
 
 #include <Drivers/DeviceDriver.hpp>
 #include <Drivers/RAMDisk.hpp>
@@ -17,12 +18,21 @@
 using namespace Kernel;
 using namespace Drivers;
 
+unsigned long KernelStackBase;
+unsigned long KernelStackSize = 2*1024*1024;
+unsigned int StackReady = 0;
+
+// problem : Kernel stack is too small & overrides kernel
+// One core per 2MB *** 
+
+Kernel::MutualExclusion::SpinLock CoreSpinLock;
+
 extern "C" void Main(void) {
     __asm__ ("cli");
     SystemStructure::Initialize(); // good
     TextScreen80x25::Initialize(); // good
-    DescriptorTables::Initialize(); // error?
     MemoryManagement::Initialize();
+    DescriptorTables::Initialize(); // error?
     // PIT::Initialize();
     Keyboard::Initialize();
     Mouse::Initialize();
@@ -46,20 +56,30 @@ extern "C" void Main(void) {
     
     TaskManagement::Initialize();
     
-    /*
     LocalAPIC::Timer::Initialize();
+    LocalAPIC::GlobalEnableLocalAPIC();
     LocalAPIC::EnableLocalAPIC();
-    IOAPIC::Initialize();
+    IOAPIC::InitializeRedirectionTable();
+    CoreSpinLock.Initialize();
     LocalAPIC::ActiveAPCores();
-    */
-    
-    Kernel::printf("LocalAPIC Initialized\n");
+
+    // To-do : Make proper stack system
+    KernelStackBase = (unsigned long)Kernel::MemoryManagement::Allocate(KernelStackSize*CoreInformation::GetInstance()->CoreCount);
+    KernelStackBase += KernelStackSize*CoreInformation::GetInstance()->CoreCount;
+    StackReady = 1;
+
     __asm__ ("sti");
-    Kernel:printf("Enabled interrupt\n");
+    Kernel:printf("Kernel core initialized\n");
+
+    while(1) {
+        ;
+    }
+
     FileSystem::Initialize();
     Kernel::printf("File System Initialized\n");
     Drivers::StorageSystem::Initialize();
     Kernel::printf("Storage System Initialized\n");
+
     FileSystem::ISO9660::Register();
     FileSystem::FAT16::Register();
 
@@ -70,7 +90,7 @@ extern "C" void Main(void) {
     Kernel::printf("Done\n");
 
     unsigned char *Buffer = (unsigned char *)Kernel::MemoryManagement::Allocate(40960*512);
-    StorageSystem::Storage *ISORAMDisk = Drivers::RAMDisk::CreateRAMDisk(8192 , 2048 , 0x720000);
+    StorageSystem::Storage *ISORAMDisk = Drivers::RAMDisk::CreateRAMDisk(12736 , 2048 , 0x720000);
     Kernel::printf("Registering RAM disk (16MB)\n");
     FileSystem::FileInfo *VBRFile = FileSystem::ISO9660::OpenFile(ISORAMDisk , "RDIMG.IMG");
     if(VBRFile == 0x00) {
@@ -81,40 +101,34 @@ extern "C" void Main(void) {
     }
     FileSystem::ISO9660::ReadFile(ISORAMDisk , VBRFile , 16*1024*1024 , Buffer);
     StorageSystem::Storage *SysRAMDisk = Drivers::RAMDisk::CreateRAMDisk(40960 , 512 , (unsigned long)Buffer); // 8MBs of System RAM Disk, Formatted by FAT12
-    
-    Kernel::printf("rdimg.img Buffer : 0x%X\n" , Buffer);
-    struct FileSystem::FAT16::VBR VBR;
-    unsigned int TotalSector;
-    unsigned int RootDirectorySector;
-    FileSystem::FAT16::GetVBR(SysRAMDisk , &(VBR));
     Kernel::printf("Done\n");
-    /*
-    Kernel::printf("FAT Area       : %d\n" , FileSystem::FAT16::GetFATAreaLocation(&(VBR)));
-    Kernel::printf("Root Directory : %d\n" , FileSystem::FAT16::GetRootDirectoryLocation(&(VBR)));
-    Kernel::printf("Sectors per cluster : %d\n" , VBR.SectorsPerCluster);
-    TotalSector = (VBR.TotalSector16 == 0) ? VBR.TotalSector32 : VBR.TotalSector16;
-    Kernel::printf("TotalSector    : %d\n" , TotalSector);
-    RootDirectorySector = ((VBR.RootDirectoryEntryCount*32)+(VBR.BytesPerSector-1))/VBR.BytesPerSector;
-    Kernel::printf("Root Directory Size : %d\n" , RootDirectorySector);
-    Kernel::printf("FAT Size       : %d\n" ,((TotalSector-(VBR.ReservedSectorCount+RootDirectorySector))+((256*VBR.SectorsPerCluster)+VBR.NumberOfFAT-1))/(VBR.NumberOfFAT+(256*VBR.SectorsPerCluster)));
-    */
     FileSystem::FileInfo *FileInfo = FileSystem::FAT16::OpenFile(SysRAMDisk , "Hello.txt");
     if(FileInfo == 0x00) {
         Kernel::printf("File \"Hello.txt\" not found.\n");
     }
-    FileSystem::FileInfo *FileInfo2 = FileSystem::FAT16::OpenFile(SysRAMDisk , "Hello.txt");
+    FileSystem::FileInfo *FileInfo2 = FileSystem::FAT16::OpenFile(SysRAMDisk , "Testing/Hello.txt");
     if(FileInfo2 == 0x00) {
         Kernel::printf("File \"Testing/Hello.txt\" not found.\n");
     }
+
     char Data[FileInfo2->FileSize+1] = {0 , };
     FileSystem::FAT16::ReadFile(SysRAMDisk , FileInfo2 , FileInfo2->FileSize , Data);
     Kernel::printf("Data : \n");
     Kernel::printf("%s\n" , Data);
     Kernel::printf("===================================\n");
-
     // To-do : Create interface
     while(1) {
-        ;
+        Kernel::printf("%c" , Kernel::Keyboard::GetASCIIData());
+    }
+}
+
+void TestTask(void) {
+    unsigned char *VideoMemory = (unsigned char *)(0xB8000+(TaskManagement::GetCurrentlyRunningTaskID()*2));
+    char haha[4] = {'-' , '\\' , '|' , '/'};
+    unsigned char i = 0;
+    while(1) {
+        *(VideoMemory) = (i++);
+        Kernel::PIT::DelayMicroseconds(100);
     }
 }
 
@@ -124,9 +138,14 @@ extern "C" void APStartup(void) {
      * Create Memory Management System
      * Create IO Redirection table
      */
-    unsigned long i = 0;
-    unsigned long CoreID = LocalAPIC::GetCurrentAPICID();
+    __asm__ ("cli");
+    DescriptorTables::Initialize();
+    LocalAPIC::EnableLocalAPIC();
+    LocalAPIC::Timer::Initialize();
+    __asm__ ("sti");
     while(1) {
-        __asm__ ("hlt");
+        ;
     }
 }
+
+// To-do : More reliable way to detect core
