04aeba4c1dfa229a309d630726a0ff2acae69057
Juha3141 <ianisnumber2027@gmail.com>
2023-05-08 15:48:57 +0900

    Fixed bugs, added mutual exclusion

--- Kernel64/Sources/MutualExclusion.asm
+++ Kernel64/Sources/MutualExclusion.asm
@@ -2,3 +2,15 @@
 
 SECTION .text
 
+global _cmpxchg
+
+_cmpxchg:
+    mov rax , rsi
+    lock cmpxchg byte[rdi] , dl
+    je .L1
+    xor rax , rax
+    ret
+
+.L1:
+    mov rax , 1
+    ret
--- Kernel64/Sources/MutualExclusion.cpp
+++ Kernel64/Sources/MutualExclusion.cpp
@@ -1,15 +1,38 @@
 #include <MutualExclusion.hpp>
-#include <APIC.hpp>
 
-void Kernel::MutualExclusion::SpinLock::Initialize(void) {
-    return;
+void Kernel::MutualExclusion::SpinLock::Initialize(void)  {
+    Locked = 0;
+    LockedCoreID = 0xFFFFFFFF;
+    LockedCount = 0;
 }
 
 void Kernel::MutualExclusion::SpinLock::Lock(void) {
-    
-    return;
+    if(!_cmpxchg(&(Locked) , 0 , 1)) {
+        if(LockedCoreID == LocalAPIC::GetCurrentAPICID()) {
+            // multiple lock
+            LockedCount++;
+            return;
+        }
+        while(!_cmpxchg(&(Locked) , 0 , 1)) {
+            while(Locked == 1) {
+                __asm__ __volatile__ ("pause":::"memory");
+            }
+        }
+    }
+    // First lock
+    LockedCount = 1;
+    LockedCoreID = LocalAPIC::GetCurrentAPICID();
 }
 
-void Kernel::MutualExclusion::SpinLock::Unlock(void) {
-    return;
+void Kernel::MutualExclusion::SpinLock::Unlock(void)  {
+    if((Locked == 0)||(LockedCoreID != LocalAPIC::GetCurrentAPICID())) {
+        return;
+    }
+    if(LockedCount > 1) {
+        LockedCount -= 1;
+        return;
+    }
+    LockedCoreID = 0xFFFFFFFF;
+    Locked = 0;
+    LockedCount = 0;
 }
