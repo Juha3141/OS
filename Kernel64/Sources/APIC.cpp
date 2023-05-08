308b2a948fcb2da6c3f9671ff285daaee0e967a0
Juha3141 <ianisnumber2027@gmail.com>
2023-05-08 15:46:36 +0900

    Added SpinLock, fixed bugs in multicore activate system

--- Kernel64/Sources/APIC.cpp
+++ Kernel64/Sources/APIC.cpp
@@ -35,6 +35,8 @@ unsigned int Kernel::IOAPIC::ReadRegister(unsigned char RegisterAddress) {
     return *((unsigned int *)(CoreInformation->IOAPICAddress+IOAPIC_REGISTER_WINDOW));
 }
 
+static Kernel::MutualExclusion::SpinLock TimerSpinLock;
+
 void Kernel::LocalAPIC::GlobalEnableLocalAPIC(void) {
     // Disable all interrupts
     IO::Write(0x21 , 0xFF);
@@ -55,9 +57,7 @@ void Kernel::LocalAPIC::EnableLocalAPIC(void) {
     // Kernel::printf("LocalAPIC Register Address from the structure : 0x%X\n" , CoreInformation->LocalAPICAddress);
     // To enable APIC, set bit 8 in LocalAPIC Spurious interrupt vector register to 1 
     // Currently, we're enabling BSP's Local APIC
-    WriteRegister(LAPIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER , ReadRegister(LAPIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER)|0x100);
-    // For debugging, read the register again
-    // Kernel::printf("LAPIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER      : 0x%X\n" , ReadRegister(LAPIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER));
+    WriteRegister(LAPIC_LVT_SPURIOUS_REGISTER , ReadRegister(LAPIC_LVT_SPURIOUS_REGISTER)|0x100);
 }
 
 bool Kernel::LocalAPIC::CheckBSP(void) {    // Check if current CPU is BSP
@@ -65,23 +65,18 @@ bool Kernel::LocalAPIC::CheckBSP(void) {    // Check if current CPU is BSP
     // MSR 0x1B : APIC_BASE
     // Check if bit 8, which is BSP bit, is enabled.
     // If it is, currently running core is BSP, it it isn't currently running core is AP.
+    TimerSpinLock.Lock();
     __asm__ ("mov rcx , 0x1B");
     __asm__ ("rdmsr");
     __asm__ ("mov %0 , eax":"=r"(EAX)); // We just need lower 32bits of the register
+    TimerSpinLock.Unlock();
     if((EAX & 0b100000000) == 0b100000000) {
         return true;
     }
-    else {
-        return false;
-    }
+    return false;
 }
 
 unsigned int ActivatedCoreCount = 0;
-/*
-void Kernel::LocalAPIC::SendActivatedSignal(void) {
-    CoreActivated = 1;          // AP sends this signal, and BSP gets it
-    ActivatedCoreCount++;
-}*/
 
 unsigned int Kernel::LocalAPIC::GetActivatedCoreCount(void) {
     return ActivatedCoreCount;
@@ -92,7 +87,7 @@ unsigned int Kernel::LocalAPIC::GetCurrentAPICID(void) {
     return (unsigned int)(Kernel::LocalAPIC::ReadRegister(LAPIC_ID_REGISTER) >> 24);
 }
 
-void Kernel::LocalAPIC::ActiveAPCores(void) {
+void Kernel::LocalAPIC::ActivateAPCores(void) {
     int i;
     int j;
     int ActivatedCoreCount;
@@ -109,7 +104,6 @@ void Kernel::LocalAPIC::ActiveAPCores(void) {
     // Since it's confirmed that we're running in BSP core, by simply getting the current APIC ID, we can get the BSP core APIC ID.
     BSPAPICID = GetCurrentAPICID();
     
-    __asm__ ("cli");
     // Level Trigger : For INIT IPI
     // Level Trigger , Delivery Mode : INIT , Sending Level : Assert , Using Abbreviation , For every core
     WriteRegister(LAPIC_ERROR_STATUS_REGISTER , 0x00);
@@ -119,7 +113,6 @@ void Kernel::LocalAPIC::ActiveAPCores(void) {
     WriteRegister(LAPIC_INTERRUPT_COMMAND_REGISTER , LAPIC_ICR_IPI_INIT|LAPIC_ICR_LEVEL_ASSERT|LAPIC_ICR_SENDING_FOR_EVERYONE_EXCEPT_FOR_ME);
     WriteRegister(0x310 , ReadRegister(0x310)|((CoreInformation->LocalAPICID[i] & 0b1111) << 24));
     // Wait 10ms for the response
-    Kernel::PIT::DelayMilliseconds(10);
     // If Local APIC status is pending, sending IPI is failed. :/
     while((ReadRegister(LAPIC_INTERRUPT_COMMAND_REGISTER) & LAPIC_ICR_SENT_STATUS_PENDING) == LAPIC_ICR_SENT_STATUS_PENDING) {
         // Kernel::printf("Failed sending Init IPI for : LAPIC 0x%X, Processor 0x%X\n" , CoreInformation->LocalAPICID[i] , CoreInformation->LocalAPICProcessorID[i]);
@@ -134,7 +127,6 @@ void Kernel::LocalAPIC::ActiveAPCores(void) {
         }
     }
     
-    //__asm__ ("sti");
     while(1) { // Wait until activated core count is equal to number of cores
         ActivatedCoreCount = GetActivatedCoreCount();
         Kernel::PIT::DelayMilliseconds(10);
@@ -192,8 +184,9 @@ unsigned int Kernel::LocalAPIC::Timer::GetInitialValue(void) {
 }
 
 void Kernel::LocalAPIC::Timer::MainInterruptHandler(void) {
-    // Kernel::TaskManagement::SwitchTaskInTimerInterrupt();
-    Kernel::LocalAPIC::SendEOI();
+    // even I synchronize this it keeps crashing
+    Kernel::TaskManagement::SwitchTaskInTimerInterrupt();
+    // Kernel::LocalAPIC::SendEOI();
 }
 
 /// I/O APIC ///
@@ -246,7 +239,6 @@ void Kernel::IOAPIC::InitializeRedirectionTable(void) {
                 break;
         };
     }
-    
 }
 
 void Kernel::IOAPIC::WriteIORedirectionTable(int INTIN , struct IORedirectionTable RedirectionTable) {
