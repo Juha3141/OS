7941ba2eea0886bcf555c117ae3206f874891450
Juha3141 <ianisnumber2027@gmail.com>
2023-05-11 14:26:26 +0900

    Removed some unnecessary features

--- Kernel64/Sources/Main.cpp
+++ Kernel64/Sources/Main.cpp
@@ -6,10 +6,11 @@
 #include <Paging.hpp>
 #include <MutualExclusion.hpp>
 
-#include <Drivers/DeviceDriver.hpp>
+#include <Drivers/StorageDriver.hpp>
+#include <Drivers/FileSystemDriver.hpp>
+
 #include <Drivers/RAMDisk.hpp>
-#include <Drivers/PATA.hpp>
-#include <Drivers/PATA_CD.hpp>
+#include <Drivers/IDE.hpp>
 #include <Drivers/PCI.hpp>
 
 #include <FileSystem/MBR.hpp>
@@ -19,7 +20,6 @@
 #include <FileSystem/FAT16.hpp>
 
 using namespace Kernel;
-using namespace Drivers;
 
 unsigned long KernelStackBase = 0;
 unsigned long KernelStackSize = 2*1024*1024;
@@ -27,7 +27,11 @@ unsigned long KernelStackSize = 2*1024*1024;
 // problem : Kernel stack is too small & overrides kernel
 // One core per 2MB *** 
 
-void AutoFormat_FAT16(struct StorageSystem::Storage *Storage);
+// problem 2 : Using traditional malloc-like system crashes when using abstract class
+// -> use new/delete keyword.
+
+void InitializeDrive_MBR(struct Storage *Storage);
+void CreatePartition_FAT16(struct Storage *Storage , const char *VolumeLabel , unsigned long StartAddress , unsigned long Size);
 
 extern "C" void Main(void) {
     __asm__ ("cli");
@@ -70,87 +74,95 @@ extern "C" void Main(void) {
 
     FileSystem::Initialize();
     Kernel::printf("File System Initialized\n");
-    Drivers::StorageSystem::Initialize();
+    StorageSystem::Initialize();
     Kernel::printf("Storage System Initialized\n");
-
-    FileSystem::ISO9660::Register();
-    FileSystem::FAT16::Register();
-
-    Drivers::PATA::Register();
-    // Drivers::PATA_CD::Register();
-    Drivers::RAMDisk::Register();
-    Kernel::printf("Detecting PCI devices\n");
+    ISO9660::Register();
+    FAT16::Register();
+    Kernel::printf("File System Registered\n");
+    Kernel::printf("RAMDiskDriver::Register : 0x%X\n" , &(RAMDiskDriver::Register));
+    
+    RAMDiskDriver::Register();
+    IDEDriver::Register();
     Drivers::PCI::Detect();
-    Kernel::printf("Done\n");
+    struct Storage *RAMDiskStorage = RAMDiskDriver::CreateRAMDisk(65536 , 512 , 0x00);
+    Kernel::printf("Registered RAMDisk Storage , Location : 0x%X\n" , RAMDiskStorage->PhysicalInfo.Resources[0]);
+    
     // To-do : Create interface
-    Drivers::StorageSystem::Storage *RAMDiskStorage = Drivers::RAMDisk::CreateRAMDisk(65536 , 512 , 0x00);
-    Kernel::printf("Created Live RAM Disk (Size : %dMB)\n" , RAMDiskStorage->Geometry.BytesPerSector*RAMDiskStorage->Geometry.TotalSectorCount/1024/1024);
-    Kernel::printf("Location : 0x%X\n" , RAMDiskStorage->Resources[0]);
     // AutoFormat_FAT16(Storage);
-
+    
+    // Let's do some cleaning
     // To-do : Create function that creates partition
-    AutoFormat_FAT16(RAMDiskStorage);
+    struct Storage *Storage = StorageSystem::SearchStorage("idehd" , 0);
+    if(Storage == 0x00) {
+        Kernel::printf("Not found.\n");
+        while(1) {
+            ;
+        }
+    }
+    InitializeDrive_MBR(Storage);
+    CreatePartition_FAT16(Storage , "NO NAME    " , 128 , 65550-128);
     while(1) {
         ;
     }
 }
 
-void AutoFormat_FAT16(struct StorageSystem::Storage *Storage) {
-    struct FileSystem::FAT16::VBR VBR;
-    unsigned char ByteCode[140] = {
-        0xB8 , 0x00 , 0x00 , 0x8E , 0xD8 , 0xB8 , 0x00 , 0xB8 , 0x8E , 0xC0 , 0x31 , 0xFF , 0x26 , 0xC6 , 0x05 , 0x00 , 
-        0x47 , 0x26 , 0xC6 , 0x05 , 0x07 , 0x47 , 0x81 , 0xFF , 0xA0 , 0x0F , 0x72 , 0xF0 , 0x26 , 0x66 , 0xC7 , 0x06 , 
-        0x00 , 0x00 , 0x48 , 0x07 , 0x6F , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x04 , 0x00 , 0x77 , 0x07 , 0x20 , 0x07 , 
-        0x26 , 0x66 , 0xC7 , 0x06 , 0x08 , 0x00 , 0x64 , 0x07 , 0x69 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x0C , 0x00 , 
-        0x64 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x10 , 0x00 , 0x79 , 0x07 , 0x6F , 0x07 , 0x26 , 0x66 , 
-        0xC7 , 0x06 , 0x14 , 0x00 , 0x75 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x18 , 0x00 , 0x67 , 0x07 , 
-        0x65 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x1C , 0x00 , 0x74 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 
-        0x20 , 0x00 , 0x68 , 0x07 , 0x65 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x24 , 0x00 , 0x72 , 0x07 , 0x65 , 0x07 , 
-        0x26 , 0x66 , 0xC7 , 0x06 , 0x28 , 0x00 , 0x3F , 0x07 , 0x00 , 0x00 , 0xEB , 0xFE , };
+const unsigned char CommonByteCode[140] = {
+    0xB8 , 0x00 , 0x00 , 0x8E , 0xD8 , 0xB8 , 0x00 , 0xB8 , 0x8E , 0xC0 , 0x31 , 0xFF , 0x26 , 0xC6 , 0x05 , 0x00 , 
+    0x47 , 0x26 , 0xC6 , 0x05 , 0x07 , 0x47 , 0x81 , 0xFF , 0xA0 , 0x0F , 0x72 , 0xF0 , 0x26 , 0x66 , 0xC7 , 0x06 , 
+    0x00 , 0x00 , 0x48 , 0x07 , 0x6F , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x04 , 0x00 , 0x77 , 0x07 , 0x20 , 0x07 , 
+    0x26 , 0x66 , 0xC7 , 0x06 , 0x08 , 0x00 , 0x64 , 0x07 , 0x69 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x0C , 0x00 , 
+    0x64 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x10 , 0x00 , 0x79 , 0x07 , 0x6F , 0x07 , 0x26 , 0x66 , 
+    0xC7 , 0x06 , 0x14 , 0x00 , 0x75 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x18 , 0x00 , 0x67 , 0x07 , 
+    0x65 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x1C , 0x00 , 0x74 , 0x07 , 0x20 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 
+    0x20 , 0x00 , 0x68 , 0x07 , 0x65 , 0x07 , 0x26 , 0x66 , 0xC7 , 0x06 , 0x24 , 0x00 , 0x72 , 0x07 , 0x65 , 0x07 , 
+    0x26 , 0x66 , 0xC7 , 0x06 , 0x28 , 0x00 , 0x3F , 0x07 , 0x00 , 0x00 , 0xEB , 0xFE , };
+
+void InitializeDrive_MBR(struct Storage *Storage) {
+    struct FAT16::VBR VBR;
     unsigned char *BootSector = (unsigned char *)Kernel::MemoryManagement::Allocate(512);
-    struct FileSystem::FAT16::SFNEntry VolumeLabel;
-    struct StorageSystem::Partition Partition;
-    FileSystem::FAT16::WriteVBR(&(VBR) , &(Storage->Geometry));
-    memcpy(BootSector , &(VBR) , sizeof(struct FileSystem::FAT16::VBR));
-    memcpy(BootSector+sizeof(struct FileSystem::FAT16::VBR) , ByteCode , 140);
+    FAT16::WriteVBR(&(VBR) , &(Storage->PhysicalInfo.Geometry) , "POTATOOS" , "NO NAME   " , "FAT16     ");
+    memcpy(BootSector , &(VBR) , sizeof(struct FAT16::VBR));
+    memcpy(BootSector+sizeof(struct FAT16::VBR) , CommonByteCode , 140);
     BootSector[510] = 0x55;
     BootSector[511] = 0xAA;
-    Storage->Driver->WriteSectorFunction(Storage , 0 , 1 , BootSector);
-    Kernel::printf("Root Directory Location : %d\n" , FileSystem::FAT16::GetRootDirectoryLocation(&(VBR)));
-    Kernel::printf("FAT Area Location       : %d\n" , FileSystem::FAT16::GetFATAreaLocation(&(VBR)));
-    memset(&(VolumeLabel) , 0 , sizeof(struct FileSystem::FAT16::SFNEntry));
-    memcpy(VolumeLabel.FileName , "NO NAME    " , 11);
-    VolumeLabel.Attribute = 0x08;
+    Storage->Driver->WriteSector(Storage , 0 , 1 , BootSector);
+    Kernel::MemoryManagement::Free(BootSector);
+}
+
+void CreatePartition_FAT16(struct Storage *Storage , const char *VolumeLabel , unsigned long StartAddress , unsigned long Size) {
+    struct Partition Partition;
+    struct FAT16::SFNEntry VolumeLabelEntry;
     
-    Partition.StartAddressLBA = 128; // Always starts in this sector
-    Partition.EndAddressLBA = Storage->Geometry.TotalSectorCount-Partition.StartAddressLBA;
+    struct FAT16::VBR VBR;
+    struct Storage *CurrentPartition;
+    unsigned char *BootSector = (unsigned char *)Kernel::MemoryManagement::Allocate(512);
+
+    Partition.StartAddressLBA = StartAddress; // Always starts in this sector
+    Partition.EndAddressLBA = StartAddress+Size;
     Partition.PartitionType = 0x86;
     Partition.IsBootable = 0;
-// setup drive    
-    MBR::Identifier MBRIdentifier(Storage->Driver , Storage);
-    MBRIdentifier.CreatePartition(Partition);
-    StorageSystem::AddLogicalDrive(Storage->Driver , Storage , &(Partition) , 1);
-
-
-    /* <Major Problem in Kernel Core>
-     * The Faulty Memory management system(Especially "MemoryManagement::Free") was all the problem.
-     * The allocated data overwrote the system structure(In this case, it was IST), and caused triple fault.
-     * If we can't find how this happends, which is, how the memory manager failed to manage memory, we might 
-     * have to re-write this entire memory management system.
-    */
-
-    /* <What we have to do with blank disk>
-     * 1. Write MBR
-     * 2. Create new partition
-     * 3. Format the partition with some file system
-    */ 
     
-    FileSystem::FAT16::WriteClusterInfo(Storage , 0 , 0xFFF8 , &(VBR));
-    FileSystem::FAT16::WriteClusterInfo(Storage , 1 , 0xFFFF , &(VBR));
-    FileSystem::FAT16::WriteSFNEntry(Storage , FileSystem::FAT16::GetRootDirectoryLocation(&(VBR)) , &(VolumeLabel));
-    while(1) {
-        ;
-    }
+    MBR::Identifier MBRIdentifier(Storage->Driver , Storage);
+    MBRIdentifier.CreatePartition(Partition); // Physically create partition
+    StorageSystem::AddLogicalDrive(Storage->Driver , Storage , &(Partition) , 1); // Setup basic information
+    
+    CurrentPartition = Storage->LogicalStorages->GetObject(Storage->LogicalStorages->CurrentObjectCount-1);
+
+    FAT16::WriteVBR(&(VBR) , &(Storage->PhysicalInfo.Geometry) , "POTATOOS" , "NO NAME   " , "FAT16     ");
+    memcpy(BootSector , &(VBR) , sizeof(struct FAT16::VBR));
+    memcpy(BootSector+sizeof(struct FAT16::VBR) , CommonByteCode , 140);
+    BootSector[510] = 0x55;
+    BootSector[511] = 0xAA;
+    CurrentPartition->Driver->WriteSector(CurrentPartition , 0 , 1 , BootSector);
+    Kernel::MemoryManagement::Free(BootSector);
+    
+    memset(&(VolumeLabelEntry) , 0 , sizeof(struct FAT16::SFNEntry));
+    memcpy(VolumeLabelEntry.FileName , VolumeLabel , 11);
+    VolumeLabelEntry.Attribute = 0x08;
+    
+    FAT16::WriteClusterInfo(CurrentPartition , 0 , 0xFFF8 , &(VBR));
+    FAT16::WriteClusterInfo(CurrentPartition , 1 , 0xFFFF , &(VBR));
+    FAT16::WriteSFNEntry(CurrentPartition , FAT16::GetRootDirectoryLocation(&(VBR)) , &(VolumeLabelEntry));
 }
 
 extern "C" void APStartup(void) {
