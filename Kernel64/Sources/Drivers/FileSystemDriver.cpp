4decd115b1fe9e95cbfb7325a34e3aa57b78430e
Juha3141 <ianisnumber2027@gmail.com>
2023-06-03 13:11:01 +0900

    Fixed bugs

--- Kernel64/Sources/Drivers/FileSystemDriver.cpp
+++ Kernel64/Sources/Drivers/FileSystemDriver.cpp
@@ -7,10 +7,10 @@ void FileSystem::Initialize(void) {
 
 
 bool FileSystem::Register(FileSystemDriver *Driver , const char *FileSystemString) {
+    strcpy(Driver->FileSystemString , FileSystemString);
     if(FileSystemManager::GetInstance()->Register(Driver) == false) {
         return false;
     }
-    strcpy(Driver->FileSystemString , FileSystemString);
     return true;
 }
 
@@ -25,7 +25,7 @@ FileSystemDriver *FileSystem::Search(const char *FileSystemString) {
 FileSystemDriver *FileSystem::DetectFileSystem(struct Storage *Storage) {
     int i;
     FileSystemManager *Manager = FileSystemManager::GetInstance();
-    for(i = 0; i < Manager->CurrentObjectCount; i++) {
+    for(i = 0; i < Manager->Count; i++) {
         if(Manager->Check(i , Storage) == true) {
             return Manager->GetObject(i);
         }
